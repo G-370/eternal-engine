530c8b9ff2907196facaf78a971c3dc521aff8c7
Guilherme Alves Lopes <guilhermealveslopes370@gmail.com>
2023-09-13 10:36:28 -0300

    update run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/home/$USER/.pyenv/shims/python -m pip install -r requirements.txt
-/home/$USER/.pyenv/shims/python logger.py
+/home/server_utils/.pyenv/shims/python -m pip install -r requirements.txt
+/home/server_utils/.pyenv/shims/python logger.py
