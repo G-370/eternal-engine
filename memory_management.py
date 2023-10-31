79698213962a6d3b0ac10426e7f9b46a95b99128
Guilherme Alves Lopes <guilhermealveslopes370@gmail.com>
2023-10-31 11:08:54 -0300

    Adds autoclose on high memory

--- memory_management.py
+++ memory_management.py
@@ -4,11 +4,15 @@ import time
 import gc
 import discord
 import tracemalloc
+import sys
 
 import collections
 
 mems = []
 typecounts = collections.Counter()
+ONE_MEGABYTE = 1000000
+ONE_GIGABYTE = ONE_MEGABYTE * 1000
+MEMORY_LIMIT = 3 * ONE_GIGABYTE
 
 def heartbeat():
     ps = psutil.Process(os.getpid())
@@ -17,3 +21,7 @@ def heartbeat():
 
     with open('mems.output', 'at') as mfp:
         mfp.write(f'{rss}\n')
+
+    if (rss / MEMORY_LIMIT >= 1.0):
+        print("Memory pressure very high, closing off.")
+        sys.exit()
